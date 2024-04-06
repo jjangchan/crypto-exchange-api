f9847d5a6a2f3d9563dec3cb45fbd429b471019a
jjangchan <rudcks6806@gmail.com>
2024-04-06 16:13:30 +0900

    Delete cmake-build-release directory

--- cmake-build-release/unittest/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jjangchan/workspace/Clion/chan-project/crypto-exchange-api/unittest/lib
-# Build directory: /Users/jjangchan/workspace/Clion/chan-project/crypto-exchange-api/cmake-build-release/unittest/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-release/unittest/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jjangchan/workspace/Clion/chan-project/crypto-exchange-api/unittest/lib/googlemock
-# Build directory: /Users/jjangchan/workspace/Clion/chan-project/crypto-exchange-api/cmake-build-release/unittest/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
